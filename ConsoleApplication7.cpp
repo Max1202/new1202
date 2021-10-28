ac63585b5cd0daec8301a3f823495e4bb857c10c
Max1202 <melnikovmaks1202@gmail.com>
2021-10-28 16:11:48 +0300

    added system(pause) and return to new file

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	float Z, y;
+	cout << "y:" << endl;
+	cin >> y;
+	Z = 1;
+	for (int m = 1; m <= 5; m++) {
+		Z = (Z);
+	}
+	Z = tan(Z) +pow((y),5);
+	cout << "Z=" << setprecision(3) << Z << endl;
+	system("pause");
+	return 0;
+}
+
+
